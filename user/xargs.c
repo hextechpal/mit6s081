a617cb61697bd3dd3f3e5ff4ee5f6a1035224659
Prashant Pal <pal.prashant31@gmail.com>
2023-12-04 21:33:55 +0530

    xargs assignment

--- user/xargs.c
+++ user/xargs.c
@@ -2,22 +2,33 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char* argv){
+int main(int argc, char* argv[]){
     if(argc < 2){
         printf("please supply a command to execute");
+	exit(0);
     }
-
+	
     char buf[512];
-    while ((int b = read(1, &buf, sizeof(buf))) > 0){
+    int b;
+    while ((b=read(0, &buf, sizeof(buf))) > 1){
+	//printf("read=%s, b=%d\n", buf, b);
         int child = fork();
         if (child == 0){
             char* eargs[argc + 1];
             int i;
-            for (i = 0; i < argc; i++){
-                strcpy(eargs[i], argv[i]);
+	    //printf("starting to copy args\n");
+            for (i = 1; i < argc; i++){
+                eargs[i-1] = argv[i];
             }
-            strcpy(eargs[i], buf);
-            memset(buf, 0, sizeof(buf));
+	    //printf("original args copied\n");
+            eargs[argc-1] = buf;
+	    eargs[argc] = 0;
+	    //printf("args array created correctly\n");
+            //memset(buf, 0, sizeof(buf));
+	    //printf("mem reset successful\n");
+	    for (int j = 0; j < argc+1; j++){
+        	printf("j=%d %s\n",j, eargs[j]);
+    	    }
             exec(eargs[0], eargs);
         }else if(child > 0){
             wait(0);
