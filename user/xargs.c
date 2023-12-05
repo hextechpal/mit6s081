51d8fb180880af15fee2f1170f97b0e10424af9c
hextechpal <pal.prashant31@gmail.com>
2023-12-05 11:22:26 +0530

    completed assignmnt 1

--- user/xargs.c
+++ user/xargs.c
@@ -2,41 +2,47 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define MAX_ARG_LEN 512
+
 int main(int argc, char* argv[]){
+    
     if(argc < 2){
         printf("please supply a command to execute");
-	exit(0);
+	    exit(0);
     }
 	
-    char buf[512];
+    char arg[MAX_ARG_LEN];
+    char buf;
+    int buf_index = 0;
+
+    char* args[argc+1];
+
     int b;
-    while ((b=read(0, &buf, sizeof(buf))) > 1){
-	//printf("read=%s, b=%d\n", buf, b);
-        int child = fork();
-        if (child == 0){
-            char* eargs[argc + 1];
-            int i;
-	    //printf("starting to copy args\n");
-            for (i = 1; i < argc; i++){
-                eargs[i-1] = argv[i];
-            }
-	    //printf("original args copied\n");
-            eargs[argc-1] = buf;
-	    eargs[argc] = 0;
-	    //printf("args array created correctly\n");
-            //memset(buf, 0, sizeof(buf));
-	    //printf("mem reset successful\n");
-	    for (int j = 0; j < argc+1; j++){
-        	printf("j=%d %s\n",j, eargs[j]);
-    	    }
-            exec(eargs[0], eargs);
-        }else if(child > 0){
-            wait(0);
+
+    for (int i = 1; i < argc; i++){
+        args[i-1] = argv[i];
+    }
+    args[argc-1] = arg;
+	args[argc] = 0;
+    
+    while ((b=read(0, &buf, 1)) > 0){
+	    
+        if(buf == '\n' || buf == ' '){
+            arg[buf_index] = 0;
+
+            int child = fork();
+            if(child == 0){
+                exec(args[0], args);
+            }else if(child > 0){
+                wait(0);
+                buf_index = 0;
+            }else{
+                fprintf(2, "cannot fork");
+            }    
         }else{
-            printf("failed to fork");
-            continue;
+            arg[buf_index] = buf;
+            buf_index++;
         }
-        
     }
     exit(0);
 }
