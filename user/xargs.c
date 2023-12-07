c039ec9aa9bb967c6925a99c342694b486b91a14
hextechpal <pal.prashant31@gmail.com>
2023-12-07 16:37:47 +0530

    formatting

--- user/xargs.c
+++ user/xargs.c
@@ -1,45 +1,57 @@
-#include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/types.h"
 #include "user/user.h"
 
 #define MAX_ARG_LEN 512
 
-int main(int argc, char* argv[]){
-    
-    if(argc < 2){
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
         printf("please supply a command to execute");
-	    exit(0);
+        exit(0);
     }
-	
+
     char arg[MAX_ARG_LEN];
     char buf;
     int buf_index = 0;
 
-    char* args[argc+1];
+    char *args[argc + 1];
 
     int b;
 
-    for (int i = 1; i < argc; i++){
-        args[i-1] = argv[i];
+    for (int i = 1; i < argc; i++)
+    {
+        args[i - 1] = argv[i];
     }
-    args[argc-1] = arg;
-	args[argc] = 0;
-    
-    while ((b=read(0, &buf, 1)) > 0){
-	    
-        if(buf == '\n' || buf == ' '){
+    args[argc - 1] = arg;
+    args[argc] = 0;
+
+    while ((b = read(0, &buf, 1)) > 0)
+    {
+
+        if (buf == '\n' || buf == ' ')
+        {
             arg[buf_index] = 0;
 
             int child = fork();
-            if(child == 0){
+            if (child == 0)
+            {
                 exec(args[0], args);
-            }else if(child > 0){
+            }
+            else if (child > 0)
+            {
                 wait(0);
                 buf_index = 0;
-            }else{
+            }
+            else
+            {
                 fprintf(2, "cannot fork");
-            }    
-        }else{
+            }
+        }
+        else
+        {
             arg[buf_index] = buf;
             buf_index++;
         }
